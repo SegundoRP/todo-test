12f281f1a294c4078ca6b60bd86c7a508f03221d
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-10-17 23:47:04 -0500

    configure to root when user cant access to a view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_locale
 
+  rescue_from CanCan::AccessDenied do |_exception|
+    redirect_to root_path
+  end
+
   def set_locale
     I18n.locale = 'es'
   end
