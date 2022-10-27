90fc27fbf0a073f0177cc906fcc48c305f452873
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-10-26 22:44:20 -0500

    create job with the service object

--- app/jobs/tasks/send_email_job.rb
+++ app/jobs/tasks/send_email_job.rb
@@ -1,7 +1,11 @@
 class Tasks::SendEmailJob
   include SuckerPunch::Job
 
-  def perform
-    raise NotImplementedError
+  def perform(task_id)
+    task = Task.find(task_id)
+    Tasks::SendEmail.new.call task
   end
 end
+
+# siempre es mejor pasar strings o integer en el perform ya que en los jobs
+# y en background puede que no funcione bien la serializacion si le pasamos objetos
