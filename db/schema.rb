4d771e31a08ae9dda6ff43b3f1534371a1334edf
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-10-27 23:41:59 -0500

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_28_040143) do
+ActiveRecord::Schema.define(version: 2022_10_28_044014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2022_10_28_040143) do
     t.bigint "owner_id", null: false
     t.string "code"
     t.string "state"
+    t.text "transitions", default: [], array: true
     t.index ["category_id"], name: "index_tasks_on_category_id"
     t.index ["owner_id"], name: "index_tasks_on_owner_id"
   end
