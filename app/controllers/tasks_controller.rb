c7b4950434de5b60bbbdd61e48760a1fa813dbb1
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-10-30 23:06:47 -0500

    create trigger method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -63,6 +63,10 @@ class TasksController < ApplicationController
     end
   end
 
+  def trigger
+    Tasks::TriggerEvent.new.call
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_task
